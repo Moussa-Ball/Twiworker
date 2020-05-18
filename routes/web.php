06c31792bb39076211f8a0351331d796bb692406
Moussa Ball <moiseball20155@gmail.com>
2020-05-18 16:20:34 +0000

    Add PHP_CodeSniffer and Laravel UI for Auth & Vue.js

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Authentification routes.
+Auth::routes(['verify' => true]);
 
-Route::get('/', function () {
-    return view('welcome');
+/*
+*--------------------------------------------------------------------------
+* GUEST ROUTES
+*--------------------------------------------------------------------------
+*/
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+});
+
+/*
+*--------------------------------------------------------------------------
+* AUTH ROUTES
+*--------------------------------------------------------------------------
+*/
+Route::group(['middleware' => ['auth', 'verified']], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
 });
