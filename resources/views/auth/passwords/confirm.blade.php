16a413ea69929ac298e9d5c8b23981f3180e5c5d
Moussa Ball <moiseball20155@gmail.com>
2020-08-27 13:46:44 +0000

    Conrim Password page completed

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,28 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mx-auto">
-        <div class="flex flex-wrap justify-center">
-            <div class="w-full max-w-sm">
-                <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
-
-                    <div class="font-semibold bg-gray-200 text-gray-700 py-3 px-6 mb-0">
+    <div class="container mx-auto flex justify-center items-center">
+         <div class="flex flex-wrap justify-center">
+            <div class="md:w-400px lg:w-400px w-auto mx-3">
+                <div class="my-16 flex flex-col break-words bg-white rounded-none shadow-dropdown">
+                    <div class="font-semibold text-gray-700 py-3 px-6 mb-0 uppercase self-center mt-6">
                         {{ __('Confirm Password') }}
                     </div>
 
                     <form class="w-full p-6" method="POST" action="{{ route('password.confirm') }}">
                         @csrf
 
-                        <p class="leading-normal">
+                        <p class="text-orange-500 text-sm italic -mt-3 mb-6 text-center">
                             {{ __('Please confirm your password before continuing.') }}
                         </p>
 
-                        <div class="flex flex-wrap my-6">
-                            <label for="password" class="block text-gray-700 text-sm font-bold mb-2">
-                                {{ __('Password') }}:
-                            </label>
-
-                            <input id="password" type="password" class="form-input w-full @error('password') border-red-500 @enderror" name="password" required autocomplete="new-password">
+                        <div class="flex flex-wrap mb-6">
+                            <input id="password" type="password" class="form-input w-full @error('password') border-red-500 @enderror text-sm rounded-none shadow-none" name="password" required placeholder="Password">
 
                             @error('password')
                                 <p class="text-red-500 text-xs italic mt-4">
@@ -32,14 +27,17 @@
                         </div>
 
                         <div class="flex flex-wrap items-center">
-                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-gray-100 font-bold  py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                                {{ __('Confirm Password') }}
+                            <button type="submit" class="transitionn duration-300 shadow-dropdown hover:shadow-btn-hover bg-primary hover:bg-opacity-95 text-gray-100 font-medium py-3 px-4 rounded-none text-sm focus:outline-none w-full uppercase">
+                                {{ __('Comfirm Password') }}
                             </button>
 
                             @if (Route::has('password.request'))
-                                <a class="text-sm text-blue-500 hover:text-blue-700 whitespace-no-wrap no-underline ml-auto" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
+                            <div class="w-full border-b my-6"></div>
+                            <p class="w-full text-sm text-center text-gray-800 mb-2">
+                                <a class="text-primary hover:text-opacity-75 no-underline" href="{{ route('password.request') }}">
+                                    {{ __('Forgot your password?') }}
                                 </a>
+                            </p>
                             @endif
                         </div>
                     </form>
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,6 @@ Route::group(['middleware' => 'guest'], function () {
 * AUTH ROUTES
 *--------------------------------------------------------------------------
 */
-Route::group(['middleware' => ['auth', 'verified']], function () {
+Route::group(['middleware' => ['auth', 'verified', 'password.confirm']], function () {
     Route::get('/feed', 'JobController@index')->name('jobs.index');
 });
